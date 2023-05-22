3c23c18396088f3684a87964f9dc142e47e73a0e
Andrey Musinov <andrew.musinov@gmail.com>
2023-05-22 18:07:13 +0100

    Исправил все ошибки после 2 тестов

--- models/card.js
+++ models/card.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const regex = require('../utils/constants');
+const { regex } = require('../utils/constants');
 
 const cardSchema = new Schema(
   {
